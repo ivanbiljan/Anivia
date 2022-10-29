dae5b1671eb24aae1c411c25a876340d69db8cd2
ivanbiljan <ssbiljan@gmail.com>
2022-10-29 14:31:15 +0200

    Test

--- AniviaWeb2/RenderKeepAliveTask.cs
+++ AniviaWeb2/RenderKeepAliveTask.cs
@@ -8,7 +8,7 @@ public static class RenderKeepAliveTask
 {
     private static readonly HttpClient HttpClient = new();
     private static readonly CancellationTokenSource CancellationTokenSource = new();
-    private static readonly PeriodicTimer Timer = new PeriodicTimer(TimeSpan.FromMinutes(1));
+    private static readonly PeriodicTimer Timer = new PeriodicTimer(TimeSpan.FromSeconds(15));
     private static Task? _handler;
 
     public static void Start(DiscordSocketClient discordSocketClient) =>
@@ -18,14 +18,8 @@ public static class RenderKeepAliveTask
     {
         while (await Timer.WaitForNextTickAsync(CancellationTokenSource.Token))
         {
-            var aniviaRole = discordSocketClient.Guilds.First().GetRole(1032015477054115862);
-            await aniviaRole.ModifyAsync(
-                r =>
-                {
-                    r.Color = new Color(Random.Shared.Next(255), Random.Shared.Next(255), Random.Shared.Next(255));
-                });
-
-            await HttpClient.GetAsync("https://anivia.onrender.com");
+            var response = await HttpClient.GetAsync("https://anivia.onrender.com");
+            Console.WriteLine($"Render ping returned {response.StatusCode}");
         }
     }
 }
