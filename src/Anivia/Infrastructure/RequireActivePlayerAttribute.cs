b4a60d3d778cfc96a1834ebfd3d3440003607a65
Ivan <ssbiljan@gmail.com>
2025-12-24 14:26:39 +0100

    Add RequireUserInVoiceChannelAttribute

--- src/Anivia/Infrastructure/RequireActivePlayerAttribute.cs
+++ src/Anivia/Infrastructure/RequireActivePlayerAttribute.cs
@@ -3,9 +3,13 @@ using Victoria;
 
 namespace Anivia.Infrastructure;
 
-internal sealed class RequiresActivePlayerAttribute : PreconditionAttribute
+internal sealed class RequireActivePlayerAttribute : PreconditionAttribute
 {
-    public override async Task<PreconditionResult> CheckPermissionsAsync(ICommandContext context, CommandInfo command, IServiceProvider services)
+    public override async Task<PreconditionResult> CheckPermissionsAsync(
+        ICommandContext context,
+        CommandInfo command,
+        IServiceProvider services
+    )
     {
         var lavaNode = services.GetRequiredService<LavaNode<LavaPlayer<LavaTrack>, LavaTrack>>();
         if (await lavaNode.TryGetPlayerAsync(context.Guild.Id) is null)
---
+++ src/Anivia/Infrastructure/RequireUserInVoiceChannelAttribute.cs
@@ -0,0 +1,21 @@
+using Discord.Commands;
+using Discord;
+
+namespace Anivia.Infrastructure;
+
+internal sealed class RequireUserInVoiceChannelAttribute : PreconditionAttribute
+{
+    public override async Task<PreconditionResult> CheckPermissionsAsync(
+        ICommandContext context,
+        CommandInfo command,
+        IServiceProvider services
+    )
+    {
+        if (context.User is not IVoiceState {VoiceChannel: not null})
+        {
+            return PreconditionResult.FromError("You must be in a voice channel to use this command.");
+        }
+
+        return PreconditionResult.FromSuccess();
+    }
+}
